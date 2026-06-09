f0d0d3b7916e01cb6fad7b84b644283fd5b7fb6f
MrWizard94 <mrwizard94@example.com>
2026-06-09 14:47:54 -0400

    chore: fresh start - Forge 1.20.1 project skeleton with quality gates

--- src/main/java/com/mrwizard94/biglittlefixes/BigLittleFixes.java
+++ src/main/java/com/mrwizard94/biglittlefixes/BigLittleFixes.java
@@ -1,16 +1,13 @@
 package com.mrwizard94.biglittlefixes;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 /**
